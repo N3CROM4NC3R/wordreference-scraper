8b6d9d544aa51348f3752610362543f0abd8b701
Santiago Padron <santi16648@gmail.com>
2022-08-13 09:57:35 -0400

    test: adding tests to WordreferenceScrapper class

---
+++ test/test_wordreference_scraper.py
@@ -0,0 +1,55 @@
+from wordreference_scraper import WordreferenceScraper
+
+def test_getting_one_word():
+    words = ["get"]
+    
+    wordreference_scrapper_obj = WordreferenceScraper(words)
+
+    html_word_list = wordreference_scrapper_obj.start()
+ 
+    test_file = open("./test/one_word.html","r")
+    
+    expected_output = test_file.read()
+    
+    real_output = "".join(html_word_list)
+
+    assert expected_output == real_output
+
+def test_getting_multiple_words():
+    words = ['about', 'town', 'run']
+
+    wordreference_scrapper_obj = WordreferenceScraper(words)
+
+    html_word_list = wordreference_scrapper_obj.start()
+ 
+    test_file = open("./test/multiple_words.html","r")
+
+    expected_output = test_file.read()
+    
+    real_output = "".join(html_word_list)
+
+    assert expected_output == real_output
+    
+def test_getting_sections():
+
+    words = ['Desktop', "Source"]
+
+    sections = {
+        'PT':True,
+        'AT':True,
+        'CF':False,
+        'LV':False
+    }
+
+    wordreference_scrapper_obj = WordreferenceScraper(words, sections)
+
+    html_word_list = wordreference_scrapper_obj.start()
+ 
+    test_file = open("./test/sections.html","r")
+
+    expected_output = test_file.read()
+    
+    real_output = "".join(html_word_list)
+
+    assert expected_output == real_output
+    
